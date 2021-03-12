b40a620a74ec8b19e7dbc046b2f7a3c81682aeee
Dmitry Zakhoda <68736899+ryzerrast@users.noreply.github.com>
2021-03-12 21:04:47 +0200

    file

---
+++ script.js
@@ -0,0 +1,106 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function () {
+   const form = document.querySelector('.form');
+   form.addEventListener('submit', formSend);
+
+   async function formSend(e) {
+      e.preventDefault();
+
+      let error = formValidate(form);
+
+      let formData = new FormData(document.getElementById('form'));
+      formData.append('image', formImage.files[0]);
+
+      if (error === 0) {
+         form.classList.add('_sending');
+         let response = await fetch('sendmail.php', {
+            method: 'POST',
+            body: formData
+         });
+         if (response.ok) {
+            let result = await response.json();
+            alert(result.message);
+            formPreview.innerHTML = '';
+            form.reset();
+            form.classList.remove('_sending');
+         } else {
+            alert("Ошибка HTTP: " + response.status);
+            form.classList.remove('_sending');
+         }
+      } else {
+         alert('Заполните обязательные поля');
+      }
+   }
+
+   function formValidate(form) {
+      let error = 0;
+      let formReq = document.querySelectorAll('._req');
+
+      for (let i = 0; i < formReq.length; i++) {
+         const input = formReq[i];
+         formRemoveError(input);
+
+         if (input.classList.contains('_email')) {
+            if (emailTest(input)) {
+               formAddError(input);
+               error++;
+            }
+         } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+            formAddError(input);
+            error++;
+         } else {
+            if (input.value === '') {
+               formAddError(input);
+               error++;
+            }
+         }
+      }
+      return error;
+   }
+   function formAddError(input) {
+      input.parentElement.classList.add('_error');
+      input.classList.add('_error');
+   }
+   function formRemoveError(input) {
+      input.parentElement.classList.remove('_error');
+      input.classList.remove('_error');
+   }
+   // Функция теста email
+   function emailTest(input) {
+      return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+   }
+
+   // Получаем input file в переменную
+   const formImage = document.getElementById('formImage');
+   // Получаем див для превью в переменную
+   const formPreview = document.getElementById('formPreview');
+
+   // Слушаем изменения в инпуте file
+   formImage.addEventListener('change', () => {
+      uploadFile(formImage.files[0]);
+   });
+
+   function uploadFile(file) {
+      // проверяем тип файла
+      if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+         alert('Разрешены только изображения.');
+         formImage.value = '';
+         return;
+      }
+      // проверяем размер файла
+      if (file.size > 2 * 1024 * 1024) {
+         alert('Файл должен быть менее 2 МБ.');
+         return;
+      }
+
+      var reader = new FileReader();
+      reader.onload = function (e) {
+         formPreview.innerHTML = `<img src="${e.target.result} alt="Фото">`;
+      };
+      reader.onerror = function (e) {
+         alert('Ошибка');
+      };
+      reader.readAsDataURL(file);
+   }
+});
---
+++ sendmail.php
@@ -0,0 +1,49 @@
+<?php
+
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\Exception;
+
+   require 'PHPMailer-6.3.0/src/Exception.php';
+   require 'PHPMailer-6.3.0/src/PHPMailer.php';
+
+   $mail = new PHPMailer(true);
+   $mail->CharSet = 'UTF-8';
+   $mail->setLanguage('ru', 'PHPMailer-6.3.0/language/');
+   $mail->IsHTML(true);
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   // $hand = $_POST['hand'];
+   $message = $_POST['message'];
+   $image = $_POST['image'];
+
+   // От кого письмо
+   $to = 'ryzerrast@gmail.com'
+   // Кому отправить
+   $from = trim($_POST['email']);
+
+   // if(!$mail->send()) {
+   //    $message = 'Error';
+   // } else {
+   //    $message = 'Данные отправлены!'
+   // }
+
+   // $response = ['message' => $message];
+
+   // header('Content-type: application/json');
+   // echo json_encode($response);
+
+   // Тема письма
+   $mail->Subject = 'Test'
+   $mail->Body = '' .$name . ' оставил заявку, его e-mail ' .$email;
+
+   if(!$mail->send()) {
+      $message = 'Ошибкt';
+   } else {
+      $message = 'Данные отправлены!';
+   }
+
+   $response = ['message' => $message];
+
+   header('Content-type: application/json');
+   echo json_encode($response);
+?>
